b1be6d744620036568cdd37fdc2cde0e304ddbc5
Cong Qiu <congq@imp1.cs.clemson.edu>
2016-04-08 10:59:20 -0400

    project3 added

--- project2/plymodel.h
+++ project2/plymodel.h
@@ -1,8 +1,17 @@
 #ifndef PLYMODEL_H
 #define PLYMODEL_H
 
+#include<cmath>
+#include<cstdio>
+#include<GL/gl.h>
+#include<GL/glut.h>
+
+
 class PLYModel{
 public:
 	void draw();
-	
+	GLfloat* vertexes;
+	GLint* faces;
+
+
 }
